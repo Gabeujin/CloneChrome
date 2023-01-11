ea0864bcd1e0a05c31fced124756871ae19cd874
Gabeujin <freedeveloper1993@gmail.com>
2023-01-12 00:25:30 +0900

    was Test

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
-const httpApp = require('http');
+const httpApp   = require('http');//프로토콜
+const fs        = require('fs');//파일 시스템
 
-let hostNm = '127.0.0.1';
-let port = '8081';
+const hostNm    = '127.0.0.1';
+const port      = '8081';
 
-httpApp.createServer((req, res)=>{
-    res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
-    res.end("Hello world!! feat.node.js");
+let app = httpApp.createServer((req, res)=>{
+    let url = req.url;
+    if(req.url == '/')
+    {
+        url = '/index.html';
+    }//end if
+
+    if(req.url == '/favicon.ico')
+    {
+        return res.writeHead(404);
+    }//end if
+
+    console.log(__dirname + url);
+    res.writeHead(200);
+    try{
+        res.end(fs.readFileSync(__dirname + url));
+    }catch(Exception){
+        res.end(fs.readFileSync(__dirname + '/index.html'));
+    }
 }).listen(port, hostNm);
 
 console.log(`server running ${hostNm}:${port}`);
